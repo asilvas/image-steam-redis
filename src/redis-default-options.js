d69632fcbfc8832f6e035c299305724bb7a39d05
asilvas <asilvas@godaddy.com>
2017-06-23 14:41:40 -0700

    default option optimizations based on various multi-DC testing

--- src/redis-default-options.js
+++ src/redis-default-options.js
@@ -1,10 +1,40 @@
+/**
+ * Creates a Redis Cluster instance
+ *
+ * @constructor
+ * @param {Object[]} startupNodes - An array of nodes in the cluster, [{ port: number, host: string }]
+ * @param {Object} options
+ * @param {function} [options.clusterRetryStrategy] - (times) => Math.min(100 + times * 2, 2000)
+ * @param {boolean} [options.enableOfflineQueue=true] - See Redis class
+ * @param {boolean} [options.enableReadyCheck=true] - When enabled, ioredis only emits "ready" event when `CLUSTER INFO`
+ * command reporting the cluster is ready for handling commands.
+ * @param {string} [options.scaleReads=master] - Scale reads to the node with the specified role.
+ * Available values are "master", "slave" and "all".
+ * @param {number} [options.maxRedirections=16] - When a MOVED or ASK error is received, client will redirect the
+ * command to another node. This option limits the max redirections allowed to send a command.
+ * @param {number} [options.retryDelayOnFailover=100] - When an error is received when sending a command(e.g.
+ * "Connection is closed." when the target Redis node is down),
+ * @param {number} [options.retryDelayOnClusterDown=100] - When a CLUSTERDOWN error is received, client will retry
+ * if `retryDelayOnClusterDown` is valid delay time.
+ * @param {number} [options.retryDelayOnTryAgain=100] - When a TRYAGAIN error is received, client will retry
+ * if `retryDelayOnTryAgain` is valid delay time.
+ * @param {number} [options.slotsRefreshTimeout=1000] - The milliseconds before a timeout occurs while refreshing
+ * slots from the cluster.
+ * @param {Object} [options.redisOptions] - Passed to the constructor of `Redis`.
+ * @extends [EventEmitter](http://nodejs.org/api/events.html#events_class_events_eventemitter)
+ * @extends Commander
+ */
 export default {
-  maxRedirections: 1,
-  retryDelayOnClusterDown: false,
-  retryDelayOnFailover: false,
-  retryDelayOnTryAgain: false,
+  enableOfflineQueue: true,
+  enableReadyCheck: true,
+  scaleReads: 'master',
+  maxRedirections: 2,
+  retryDelayOnFailover: 50, // `false` causes failure in some scenarios, DO NOT DISABLE!!!
+  retryDelayOnClusterDown: 100,
+  retryDelayOnTryAgain: 50,
+  slotsRefreshTimeout: 2000,
   redisOptions: {
-    connectTimeout: 1000,
+    connectTimeout: 2000,
     keyPrefix: 'isteam::'
   }
 }
